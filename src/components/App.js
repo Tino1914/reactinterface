6cc1479237cddcda46c50fe3155734158f3e3307
Konstantin Hadjiyankov <k.hadjiyankov@student.fontys.nl>
2021-05-10 15:38:46 +0200

    Finished React pet appointment system for medicine

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,8 @@ import AddAppointments from './AddAppointment';
 import SearchAppointments from './SearchAppointments';
 import ListAppointments from './ListAppointments';
 import { Component } from 'react';
-import { result, without } from 'lodash';
+import { findIndex, without } from 'lodash';
+import { each } from 'jquery';
 
 class App extends Component {
 
@@ -15,17 +16,41 @@ constructor() {
    
     myAppointments: [],
     formDisplay: false,
+    orderBy: 'petName',
+    orderDir: 'asc',
+    queryText: '',
     lastIndex: 0
   }
   this.deleteAppointment = this.deleteAppointment.bind(this);
   this.toggleForm = this.toggleForm.bind(this);
   this.addAppointment = this.addAppointment.bind(this);
+  this.changeOrder = this.changeOrder.bind(this);
+  this.searchApts = this.searchApts.bind(this);
+  this.updateInfo = this.updateInfo.bind(this);
 }
 
 toggleForm(){
   this.setState({
     formDisplay: !this.state.formDisplay
   })
+}
+
+changeOrder(order, dir) {
+  this.setState({
+    orderBy: order,
+    orderDir: dir
+  });
+}
+
+updateInfo(name, value, id) {
+  let tempApts = this.state.myAppointments;
+  let aptIndex = findIndex(this.state.myAppointments, {
+    aptId: id
+  });
+  tempApts[aptIndex][name] = value;
+  this.setState({
+    myAppointments: tempApts
+  });
 }
 
 addAppointment(apt){
@@ -38,7 +63,9 @@ addAppointment(apt){
   })
 }
 
-
+searchApts(query) {
+  this.setState({ queryText: query });
+}
 
 deleteAppointment(appointment){
   let tempApts = this.state.myAppointments;
@@ -70,6 +97,38 @@ componentDidMount(){
 
 
 render(){
+  
+  let order;
+    let filteredApts = this.state.myAppointments;
+    if (this.state.orderDir === 'asc') {
+      order = 1;
+    } else {
+      order = -1;
+    }
+
+ filteredApts = filteredApts.sort((a, b) => {
+  if (
+    a[this.state.orderBy].toLowerCase() <
+    b[this.state.orderBy].toLowerCase()
+  ) {
+    return -1 * order;
+  } else {
+    return 1 * order;
+  }
+}).filter(eachItem => {
+  return (
+    eachItem['petName']
+      .toLowerCase()
+      .includes(this.state.queryText.toLowerCase()) ||
+    eachItem['ownerName']
+      .toLowerCase()
+      .includes(this.state.queryText.toLowerCase()) ||
+    eachItem['aptNotes']
+      .toLowerCase()
+      .includes(this.state.queryText.toLowerCase())
+  );
+});
+
 
 
   return (
@@ -83,9 +142,17 @@ render(){
                 formDisplay={this.state.formDisplay}
                 toggleForm={this.toggleForm}
                 addAppointment= {this.addAppointment}/>
-                <SearchAppointments />
-               <ListAppointments appointments={this.state.myAppointments}
-               deleteAppointment={this.deleteAppointment}/>
+                <SearchAppointments
+                  orderBy={this.state.orderBy}
+                  orderDir={this.state.orderDir}
+                  changeOrder={this.changeOrder}
+                  searchApts={this.searchApts}
+                />
+                <div>
+                <h2>You can update each appointment item by clicking over it!</h2></div>
+               <ListAppointments appointments={filteredApts}
+               deleteAppointment={this.deleteAppointment}
+               updateInfo={this.updateInfo}/>
               </div>
             </div>
           </div>
--- src/components/ListAppointments.js
+++ src/components/ListAppointments.js
@@ -10,28 +10,69 @@ import Moment from 'react-moment';
             {this.props.appointments.map(item => (
               <div className="pet-item col media py-3" key={item.aptId}>
                 <div className="mr-3">
-                  <button className="pet-delete btn btn-sm btn-danger"
-                  onClick={()=> this.props.deleteAppointment(item)}>
-                  <FaTimes /></button>
+                  <button
+                    className="pet-delete btn btn-sm btn-danger"
+                    onClick={() => this.props.deleteAppointment(item)}
+                  >
+                    <FaTimes />
+                  </button>
                 </div>
     
                 <div className="pet-info media-body">
                   <div className="pet-head d-flex">
-                    <span className="pet-name">{item.petName}</span>
+                    <span
+                      className="pet-name"
+                      contentEditable
+                      suppressContentEditableWarning
+                      onBlur={e =>
+                        this.props.updateInfo(
+                          'petName',
+                          e.target.innerText,
+                          item.aptId
+                        )
+                      }
+                    >
+                     {item.petName}
+                    </span>
                     <span className="apt-date ml-auto">
-                    <Moment 
-                    date={item.aptDate}
-                    parse="YYYY-MM-dd hh:mm"
-                    format="MMM-D h:mma"
-                    />
+                      <Moment
+                        date={item.aptDate}
+                        parse="YYYY-MM-dd hh:mm"
+                        format="MMM-d h:mma"
+                      />
                     </span>
                   </div>
     
                   <div className="owner-name">
                     <span className="label-item">Owner: </span>
-                    <span>{item.ownerName}</span>
+                    <span
+                      contentEditable
+                      suppressContentEditableWarning
+                      onBlur={e =>
+                        this.props.updateInfo(
+                          'ownerName',
+                          e.target.innerText,
+                          item.aptId
+                        )
+                      }
+                    >
+                      {item.ownerName}
+                    </span>
                   </div>
-                  <div className="apt-notes">{item.aptNotes}</div>
+                  <div
+                    className="apt-notes"
+                    contentEditable
+                    suppressContentEditableWarning
+                    onBlur={e =>
+                      this.props.updateInfo(
+                        'aptNotes',
+                        e.target.innerText,
+                        item.aptId
+                      )
+                    }
+                  >
+                    {item.aptNotes}
+                  </div>
                 </div>
               </div>
             ))}
